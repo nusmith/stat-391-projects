a076d206b047028a7815ab858314841bf43a1361
nusmith <nusmith@uw.edu>
2023-05-16 10:06:30 -0700

    got plots working, still need normalization

--- hw5_main.py
+++ hw5_main.py
@@ -6,14 +6,13 @@ from scipy.stats import norm
 
 # Define densities
 def f(x):
-    if 0 <= x & x <= 1:
-        return 2 * x
+    if 0.0 <= x <= 1.0:
+        return 2.0 * x
     else:
-        return 0
-
+        return 0.0
 
 def g(x):
-    if 0 <= x & x <= 0.5:
+    if 0 <= x <= 0.5:
         return 4 * x
     elif 0.5 <= x & x <= 1:
         return 4 * (1 - x)
@@ -31,54 +30,65 @@ g_valid_nums = pd.read_csv('hw5-g-valid.dat', header=None)
 g_train = [float(i) for i in (g_train_nums.values[0])[0].split(" ")]
 g_valid = [float(i) for i in (g_valid_nums.values[0])[0].split(" ")]
 
-print("range f:" + str([min(f_train), max(f_train)]))
+# sort
+f_train.sort()
+f_valid.sort()
+g_train.sort()
+g_valid.sort()
+
 
 # Gaussian Kernel N(0,1)
 def k_gauss(x):
-    return 1/np.sqrt(2*np.pi) * np.exp(-1/2 * x**2)
+    return 1 / np.sqrt(2 * np.pi) * np.exp(-1 / 2 * x ** 2)
+
 
 # Mixture (of gaussians) density
 def fh(x, k, h, D):
     n = len(D)
     k_sum = 0
     for sample in D:
-        k_sum += k((x-sample) / h)
+        k_sum += k((x - sample) / h)
     # normalize
     return (1 / (n * h)) * k_sum
 
 
-print(fh(f_train[0], k_gauss, 0.001, f_train))
-x_d = np.arange(0, 1, 0.001)
-dens = [fh(xstep, k_gauss, 0.1, f_train) for xstep in x_d]
-#print(dens)
-# visualize the kernels
-plt.plot(x_d, dens, label = "Gaussian")
+# Likelihood of training & validation data
+# possible values for h
+h_range = [0.001, 0.002, 0.005, 0.01, 0.05, 0.1, 0.5]
+# Likelihood of the train data under fh
+Lh_series_train = np.ones(len(h_range))
+# Find likelihood of data for all values of h
+fh_train = [[fh(x, k_gauss, h0, f_train) for x in f_train] for h0 in h_range]
+Lh_series_train = [sum(np.log10(d) for d in row) for row in fh_train]
+print(Lh_series_train)
+# Visualize the KDEs
+plt.plot(f_train, fh_train[0], label="h= 0.001")
+plt.plot(f_train, fh_train[1], label="h= 0.002")
+plt.plot(f_train, fh_train[2], label="h= 0.005")
+plt.plot(f_train, fh_train[3], label="h= 0.01")
+plt.plot(f_train, fh_train[4], label="h= 0.05")
+plt.plot(f_train, fh_train[5], label="h= 0.1")
+plt.plot(f_train, fh_train[6], label="h= 0.5")
+plt.legend()
 plt.show()
 
-lh = 0
-i = 1
-# for d in f_train:
-#     lh += np.log10(fh(x=d, k=k_gauss, h=0.002, D=f_train))
-#     print(i)
-#     i += 1
-#     print(lh)
-# print(lh)
-
-# possible values for h
-h_range = [0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5]
-# Likelihood of the data (training) under fh
-# Lh_series_train = np.ones(len(h_range))
-# for i in range(len(h_range)):
-#     # Find likelihood of data for all values of h
-#     for d in f_train:
-#         Lh_series_train[i] = Lh_series_train[i] * fh(d, k_gauss, h_range[i], f_train)
-# print(Lh_series_train)
-
-# Likelihood of the data (test) under fh
+# Likelihood of the validation under fh
+# Use for Cross-Validation to find h*
 # Lh_series_test = np.ones(len(h_range))
 # for i in range(len(h_range)):
 #    h = h_range[i]
-# Find likelihood of data for all values of h
+# # Find likelihood of data for all values of h
 #    for d in f_train:
 #       Lh_series_test[i] = Lh_series_test[i] * fh(d, k_gauss, h, f_train)
 # print(Lh_series_test)
+
+
+# Plot fh*
+h_star = 0.1
+x_d = np.arange(-0.5, 1.5, 0.01)
+fh_star = [fh(x, k_gauss, h_star, f_train) for x in x_d]
+fx = [f(x_i) for x_i in x_d]
+# visualize the kernels
+plt.plot(x_d, fh_star, label="Estimated density f_h(x) for h = h*")
+plt.plot(x_d, fx, label="True density f(x)")
+plt.show()
